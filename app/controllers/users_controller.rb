147c97ec598e70ab065412fdaf09e343cde8c24d
yusai13 <future.gazer7@gmail.com>
2019-07-17 19:32:49 +0900

    resourcesを使わないCRUD処理の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,44 @@
 class UsersController < ApplicationController #クラス名は大文字から始める。
   def index
-    @message = "Hello World!"
+    @users = User.all
+  end
+
+  # 新規登録画面に対応するアクション
+  def new
+    @user = User.new
+  end
+
+  # 新規登録するためのアクション
+  def create
+    User.create(user_params)
+    redirect_to action: 'index'
+  end
+
+  # 編集するためのアクション
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  #更新するためのアクション
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to action: 'index'
+  end
+
+  # 削除するためのアクション
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+    redirect_to action: 'index'
+  end
+
+  #このクラスだけで使うアクションを定義
+
+  private
+
+  #データベースに登録するデータを制限する
+  def user_params
+    params.require(:user).permit(:name, :age)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get '/intro', to: 'intro#index'
+  # 一覧画面へのルーティング
   get '/users', to: 'users#index'
+  # 新規登録画面へのルーティング
+  get '/users/new', to: 'users#new'
+  # 新規登録を行うためのルーティング
+  post '/users', to: 'users#create'
+  # 編集画面へのルーティング
+  get '/users/:id/edit', to: 'users#edit'
+  # 更新を行うためのルーティング
+  patch '/users/:id', to: 'users#update'
+  # 削除を行うためのルーティング
+  delete '/users/:id', to: 'users#destroy'
 end
