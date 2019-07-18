d8153eb8e5bca5d3d1ee756bde2e8b33329d529b
yusai13 <future.gazer7@gmail.com>
2019-07-18 12:34:29 +0900

    resourcesを使ったCRUD処理の実装とBootstrap&jQueryの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   get '/intro', to: 'intro#index'
-  # 一覧画面へのルーティング
-  get '/users', to: 'users#index'
-  # 新規登録画面へのルーティング
-  get '/users/new', to: 'users#new'
-  # 新規登録を行うためのルーティング
-  post '/users', to: 'users#create'
-  # 編集画面へのルーティング
-  get '/users/:id/edit', to: 'users#edit'
-  # 更新を行うためのルーティング
-  patch '/users/:id', to: 'users#update'
-  # 削除を行うためのルーティング
-  delete '/users/:id', to: 'users#destroy'
+  get '/table', to: 'table#index'
 end
